620c69e35a204ccdd7d80d7b9e2914cd0f13ca0e
juanpablo <juanpablo.canepa21@gmail.com>
2020-08-18 14:24:20 -0500

    Se actualiza servicio de clientes, se añade tipo de persona

---
+++ src/main/java/pe/com/aldesa/aduanero/constant/PersonType.java
@@ -0,0 +1,8 @@
+package pe.com.aldesa.aduanero.constant;
+
+public class PersonType {
+
+	public static final Integer LEGAL = 1;
+
+	public static final Integer NATURAL = 2;
+}
--- src/main/java/pe/com/aldesa/aduanero/service/ClienteService.java
+++ src/main/java/pe/com/aldesa/aduanero/service/ClienteService.java
@@ -12,6 +12,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import pe.com.aldesa.aduanero.constant.ApiError;
+import pe.com.aldesa.aduanero.constant.PersonType;
 import pe.com.aldesa.aduanero.dto.ApiResponse;
 import pe.com.aldesa.aduanero.entity.Cliente;
 import pe.com.aldesa.aduanero.entity.Empresa;
@@ -25,21 +26,21 @@ import pe.com.aldesa.aduanero.repository.TipoPersonaRepository;
 
 @Service
 public class ClienteService {
-	
+
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
-	
+
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
+
 	@Autowired
 	private TipoPersonaRepository tipoPersonaRepository;
-	
+
 	@Autowired
 	private PersonaRepository personaRepository;
-	
+
 	@Autowired
 	private EmpresaRepository empresaRepository;
-	
+
 	public ApiResponse findAll() throws ApiException {
 		List<Cliente> clientes = clienteRepository.findAll();
 		int total = clientes.size();
@@ -49,7 +50,7 @@ public class ClienteService {
 		}
 		return ApiResponse.of(ApiError.SUCCESS.getCode(), ApiError.SUCCESS.getMessage(), clientes, total);
 	}
-	
+
 	public ApiResponse findById(Long id) throws ApiException {
 		Cliente tmpCliente = clienteRepository.findById(id).orElse(null);
 		logger.debug("Cliente: {}", tmpCliente);
@@ -58,55 +59,61 @@ public class ClienteService {
 		}
 		return ApiResponse.of(ApiError.SUCCESS.getCode(), ApiError.SUCCESS.getMessage(), tmpCliente);
 	}
-	
+
 	public ApiResponse save(String request) throws ApiException {
 		Cliente responseCliente;
-		
+
 		JsonNode root;
 		Integer	idTipoPersona = null;
 		Long idPersona = null;
 		Long idEmpresa = null;
 		try {
 			root = new ObjectMapper().readTree(request);
-			
+
 			idTipoPersona = root.path("idTipoPersona").asInt();
 			logger.debug("idTipoPersona: {}", idTipoPersona);
-			
+
 			idPersona = root.path("idPersona").asLong();
 			logger.debug("idPersona: {}", idPersona);
-			
+
 			idEmpresa = root.path("idEmpresa").asLong();
 			logger.debug("idEmpresa: {}", idEmpresa);
-			
+
 		} catch (JsonProcessingException e) {
 			throw new ApiException(ApiError.NO_APPLICATION_PROCESSED.getCode(), ApiError.NO_APPLICATION_PROCESSED.getMessage(), e.getMessage());
 		}
-		
-		if (idTipoPersona == 0 || idTipoPersona == null || idPersona == null || idPersona == 0 || idEmpresa == null || idEmpresa == 0) {
+
+		if (null == idTipoPersona || idTipoPersona == 0) {
 			throw new ApiException(ApiError.EMPTY_OR_NULL_PARAMETER.getCode(), ApiError.EMPTY_OR_NULL_PARAMETER.getMessage());
 		}
-		
+
 		logger.debug("Buscando tipo persona {}", idTipoPersona);
 		TipoPersona tipoPersona = tipoPersonaRepository.findById(idTipoPersona)
 				.orElseThrow(() -> new ApiException(ApiError.RESOURCE_NOT_FOUND.getCode(), ApiError.RESOURCE_NOT_FOUND.getMessage()));
 		logger.debug("Tipo persona {} encontrado", idTipoPersona);
-		
-		logger.debug("Buscando Persona {}", idPersona);
-		Persona persona = personaRepository.findById(idPersona)
-				.orElseThrow(() -> new ApiException(ApiError.RESOURCE_NOT_FOUND.getCode(), ApiError.RESOURCE_NOT_FOUND.getMessage()));
-		logger.debug("Persona {} encontrado", idPersona);
-		
-		logger.debug("Buscando empresa {}", idEmpresa);
-		Empresa empresa = empresaRepository.findById(idEmpresa)
-				.orElseThrow(() -> new ApiException(ApiError.RESOURCE_NOT_FOUND.getCode(), ApiError.RESOURCE_NOT_FOUND.getMessage()));
-		logger.debug("Empresa {} encontrado", idEmpresa);
-		
+
+		Persona persona = null;
+		if (PersonType.NATURAL.equals(idTipoPersona)) {
+			logger.debug("Buscando Persona {}", idPersona);
+			persona = personaRepository.findById(idPersona)
+					.orElseThrow(() -> new ApiException(ApiError.RESOURCE_NOT_FOUND.getCode(), ApiError.RESOURCE_NOT_FOUND.getMessage()));
+			logger.debug("Persona {} encontrado", idPersona);
+		}
+
+		Empresa empresa = null;
+		if (PersonType.LEGAL.equals(idTipoPersona)) {
+			logger.debug("Buscando empresa {}", idEmpresa);
+			empresa = empresaRepository.findById(idEmpresa)
+					.orElseThrow(() -> new ApiException(ApiError.RESOURCE_NOT_FOUND.getCode(), ApiError.RESOURCE_NOT_FOUND.getMessage()));
+			logger.debug("Empresa {} encontrado", idEmpresa);
+		}
+
 		try {
 			Cliente cliente = new Cliente();
 			cliente.setTipoPersona(tipoPersona);
 			cliente.setPersona(persona);
 			cliente.setEmpresa(empresa);
-			
+
 			responseCliente = clienteRepository.save(cliente);
 			logger.debug("Cliente guardado");
 		} catch (Exception e) {
@@ -114,10 +121,10 @@ public class ClienteService {
 		}
 		return ApiResponse.of(ApiError.SUCCESS.getCode(), ApiError.SUCCESS.getMessage(), responseCliente);
 	}
-	
+
 	public ApiResponse update(String request) throws ApiException {
 		Cliente responseCliente;
-		
+
 		JsonNode root;
 		Long id = null;
 		Integer	idTipoPersona = null;
@@ -125,56 +132,61 @@ public class ClienteService {
 		Long idEmpresa = null;
 		try {
 			root = new ObjectMapper().readTree(request);
-			
+
 			id = root.path("id").asLong();
 			logger.debug("id: {}", id);
-			
+
 			idTipoPersona = root.path("idTipoPersona").asInt();
 			logger.debug("idTipoPersona: {}", idTipoPersona);
-			
+
 			idPersona = root.path("idPersona").asLong();
 			logger.debug("idPersona: {}", idPersona);
-			
+
 			idEmpresa = root.path("idEmpresa").asLong();
 			logger.debug("idEmpresa: {}", idEmpresa);
-			
+
 		} catch (JsonProcessingException e) {
 			throw new ApiException(ApiError.NO_APPLICATION_PROCESSED.getCode(), ApiError.NO_APPLICATION_PROCESSED.getMessage(), e.getMessage());
 		}
-		
-		if (id == 0 || null == id || idTipoPersona == 0 || idTipoPersona == null 
-				|| idPersona == null || idPersona == 0 || idEmpresa == null || idEmpresa == 0) {
+
+		if (null == id || id == 0 || null == idTipoPersona || idTipoPersona == 0) {
 			throw new ApiException(ApiError.EMPTY_OR_NULL_PARAMETER.getCode(), ApiError.EMPTY_OR_NULL_PARAMETER.getMessage());
 		}
-		
-		boolean existsCliente = clienteRepository.existsById(id);
-		logger.debug("Existe Cliente? {}", existsCliente);
-		if (!existsCliente) {
+
+		boolean existCliente = clienteRepository.existsById(id);
+		logger.debug("Existe cliente? {} {}", id, existCliente);
+		if (!existCliente ) {
 			throw new ApiException(ApiError.RESOURCE_NOT_FOUND.getCode(), ApiError.RESOURCE_NOT_FOUND.getMessage());
 		}
-		
+
 		logger.debug("Buscando tipo persona {}", idTipoPersona);
 		TipoPersona tipoPersona = tipoPersonaRepository.findById(idTipoPersona)
 				.orElseThrow(() -> new ApiException(ApiError.RESOURCE_NOT_FOUND.getCode(), ApiError.RESOURCE_NOT_FOUND.getMessage()));
 		logger.debug("Tipo persona {} encontrado", idTipoPersona);
-		
-		logger.debug("Buscando Persona {}", idPersona);
-		Persona persona = personaRepository.findById(idPersona)
-				.orElseThrow(() -> new ApiException(ApiError.RESOURCE_NOT_FOUND.getCode(), ApiError.RESOURCE_NOT_FOUND.getMessage()));
-		logger.debug("Persona {} encontrado", idPersona);
-		
-		logger.debug("Buscando empresa {}", idEmpresa);
-		Empresa empresa = empresaRepository.findById(idEmpresa)
-				.orElseThrow(() -> new ApiException(ApiError.RESOURCE_NOT_FOUND.getCode(), ApiError.RESOURCE_NOT_FOUND.getMessage()));
-		logger.debug("Empresa {} encontrado", idEmpresa);
-		
+
+		Persona persona = null;
+		if (PersonType.NATURAL.equals(idTipoPersona)) {
+			logger.debug("Buscando Persona {}", idPersona);
+			persona = personaRepository.findById(idPersona)
+					.orElseThrow(() -> new ApiException(ApiError.RESOURCE_NOT_FOUND.getCode(), ApiError.RESOURCE_NOT_FOUND.getMessage()));
+			logger.debug("Persona {} encontrado", idPersona);
+		}
+
+		Empresa empresa = null;
+		if (PersonType.LEGAL.equals(idTipoPersona)) {
+			logger.debug("Buscando empresa {}", idEmpresa);
+			empresa = empresaRepository.findById(idEmpresa)
+					.orElseThrow(() -> new ApiException(ApiError.RESOURCE_NOT_FOUND.getCode(), ApiError.RESOURCE_NOT_FOUND.getMessage()));
+			logger.debug("Empresa {} encontrado", idEmpresa);
+		}
+
 		try {
 			Cliente cliente = new Cliente();
 			cliente.setIdCliente(id);
 			cliente.setTipoPersona(tipoPersona);
 			cliente.setPersona(persona);
 			cliente.setEmpresa(empresa);
-			
+
 			responseCliente = clienteRepository.save(cliente);
 			logger.debug("Cliente guardado");
 		} catch (Exception e) {
@@ -182,7 +194,7 @@ public class ClienteService {
 		}
 		return ApiResponse.of(ApiError.SUCCESS.getCode(), ApiError.SUCCESS.getMessage(), responseCliente);
 	}
-	
+
 	public ApiResponse delete(Long id) throws ApiException {
 		Cliente tmpCliente = clienteRepository.findById(id).orElse(null);
 		logger.debug("Cliente: {}", tmpCliente);
@@ -193,5 +205,5 @@ public class ClienteService {
 		}
 		throw new ApiException(ApiError.RESOURCE_NOT_FOUND.getCode(), ApiError.RESOURCE_NOT_FOUND.getMessage());
 	}
-	
+
 }
