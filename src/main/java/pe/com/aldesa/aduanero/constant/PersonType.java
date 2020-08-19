422040a6384bd05af0ca8590cf363d04587dc0b5
juanpablo <juanpablo.canepa21@gmail.com>
2020-08-18 22:22:51 -0500

    Añade constructor privado y estático a clase utilitaria

--- src/main/java/pe/com/aldesa/aduanero/constant/PersonType.java
+++ src/main/java/pe/com/aldesa/aduanero/constant/PersonType.java
@@ -2,6 +2,10 @@ package pe.com.aldesa.aduanero.constant;
 
 public class PersonType {
 
+	private PersonType() {
+		throw new IllegalStateException("Utility class");
+	}
+
 	public static final Integer LEGAL = 1;
 
 	public static final Integer NATURAL = 2;
