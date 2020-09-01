9596f212a5885827c11fa84ef065cb5ad9f52a77
juanpablo <juanpablo.canepa21@gmail.com>
2020-09-01 15:13:01 -0500

    Se añade mantenimiento de DAM

--- src/main/java/pe/com/aldesa/aduanero/constant/ApiError.java
+++ src/main/java/pe/com/aldesa/aduanero/constant/ApiError.java
@@ -7,7 +7,13 @@ public enum ApiError {
 	RESOURCE_NOT_FOUND("9001", "Recurso no encontrado"),
 	EMPTY_OR_NULL_PARAMETER("9002", "Uno o más parámetros están vacíos o nulos"),
 	ALREADY_EXISTS("9003", "Los datos ya han sido registrados anteriormente"),
-	MULTIPLES_SIMILAR_ELEMENTS("9004", "Hay más de 1 coincidencia (múltiples elementos encontrados)");
+	MULTIPLES_SIMILAR_ELEMENTS("9004", "Hay más de 1 coincidencia (múltiples elementos encontrados)"),
+	ADUANA_NOT_FOUND("8000", "No se encontró Aduana"),
+	AGENCIA_ADUANA_NOT_FOUND("8001", "No se encontró Agencia Aduana"),
+	DEPOSITO_TEMPORAL_NOT_FOUND("8002", "No se encontró Depósito temporal"),
+	REGIMEN_NOT_FOUND("8003", "No se encontró Régimen"),
+	TIPO_BULTO_NOT_FOUND("8004", "No se encontró Tipo Bulto"),
+	DAM_NOT_FOUND("8005", "No se encontró DAM");
 
 	private final String code;
 	private final String message;
